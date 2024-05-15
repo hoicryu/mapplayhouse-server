89427699ec484842cf6debf6aafc55226834d68c
hoicryu <hoicryu@gmail.com>
2024-05-15 23:18:32 +0900

    [Added] course_start_at column to group model

--- app/serializers/v1/group_each_serializer.rb
+++ app/serializers/v1/group_each_serializer.rb
@@ -1,5 +1,5 @@
 class V1::GroupEachSerializer < V1::BaseSerializer
-  attributes :id, :title, :musical_alias, :status, :submit_start_at, :submit_end_at, :audition_date, :performance_start_at, :performance_end_at, :application_link, :concert_hall, :main_parts
+  attributes :id, :title, :musical_alias, :status, :submit_start_at, :submit_end_at, :audition_date, :performance_start_at, :performance_end_at, :application_link, :concert_hall, :main_parts, :course_start_at
   has_one :musical, serializer: V1::MusicalSerializer
 
   def main_parts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_14_135905) do
+ActiveRecord::Schema.define(version: 2024_05_15_141414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2024_05_14_135905) do
     t.string "application_link"
     t.string "concert_hall"
     t.string "musical_alias"
+    t.datetime "course_start_at"
     t.index ["musical_id"], name: "index_groups_on_musical_id"
   end
 
