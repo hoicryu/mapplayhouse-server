3f69077b4ba2656e8e727cf70f58d10e4fa5e1b2
hoicryu <hoicryu@gmail.com>
2024-05-22 00:00:41 +0900

    [Added] part serializer to musical serializer

--- app/serializers/v1/musical_serializer.rb
+++ app/serializers/v1/musical_serializer.rb
@@ -1,5 +1,5 @@
 class V1::MusicalSerializer < V1::BaseSerializer
   include ImagableSerializer
-  # include 
   attributes :id, :title, :body, :_type
+  has_many :parts, serializer: V1::PartEachSerializer
 end
