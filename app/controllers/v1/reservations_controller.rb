437f40f52d9bc8de5e9ab26c4a182ab5727fd83a
hoicryu <hoicryu@gmail.com>
2024-06-29 17:10:09 +0900

    [Added] reservation index method

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -1,8 +1,10 @@
 class V1::ReservationsController < V1::BaseController
   def index
-    date = Date.parse(params[:date])
-    ## Reservations를 줄건데 현재 날짜 기준으로 해당 달이 포함된 예약들과 
-    ## 앞뒤 전후 1주일간의 예약들을 보내준다.
+    date = Time.zone.parse(params[:date])
+    start_date = date.beginning_of_month - 1.week
+    end_date = date.end_of_month + 1.week
+    reservations = Reservation.where('start_at >= ? AND end_at <= ?', start_date, end_date)
+    render json: reservations
   end
 
   def create
