d324bdc4d53c362148b4441afee7dd287352afa9
hoicryu <hoicryu@gmail.com>
2024-07-20 00:24:30 +0900

    [Added] reservation each serializer. [Modified] reservation create method

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -1,7 +1,7 @@
 class V1::ReservationsController < V1::BaseController
   def index
     reservations = current_api_user.reservations.order(created_at: :desc)
-    render json: reservations
+    render json: each_serialize(reservations)
   end
 
   def create
@@ -10,8 +10,11 @@ class V1::ReservationsController < V1::BaseController
     is_any_overlaps_in_start_at = Reservation.not_rejected.where("start_at <= ?", new_start_at).where("end_at >= ?", new_start_at).present?
     is_any_overlaps_in_end_at = Reservation.not_rejected.where("start_at <= ?", new_end_at).where("end_at >= ?", new_end_at).present?
     is_impossible = is_any_overlaps_in_start_at ||  is_any_overlaps_in_end_at
-    if !is_impossible 
+
+    if reservation_params[:group_id].present? && !is_impossible 
       result = current_api_user.reservations.create(reservation_params)
+    elsif !reservation_params[:group_id].present?
+      result = 'group_id_empty'
     else
       result = false
     end
@@ -23,18 +26,18 @@ class V1::ReservationsController < V1::BaseController
     start_date = date.beginning_of_month - 1.week
     end_date = date.end_of_month + 1.week
     reservations = Reservation.not_rejected.where('start_at >= ? AND end_at <= ?', start_date, end_date)
-    render json: reservations
+    render json: each_serialize(reservations)
   end
 
   def for_day
     date = Time.zone.parse(params[:date])
     reservations = Reservation.not_rejected.where("start_at >= ?", date).where("end_at <= ?", date.next_day())
-    render json: reservations
+    render json: each_serialize(reservations)
   end
 
   private
 
   def reservation_params
-      params.require(:reservation).permit(:start_at, :end_at, :num_of_people, :note)
+      params.require(:reservation).permit(:start_at, :end_at, :num_of_people, :note, :group_id)
   end
 end
