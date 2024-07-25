e8b700c8de3479a94556126e674f6e9bc4754382
hoicryu <hoicryu@gmail.com>
2024-07-26 01:45:32 +0900

    [Updated] user_groups admin

--- app/admin/user_groups.rb
+++ app/admin/user_groups.rb
@@ -6,8 +6,12 @@ ActiveAdmin.register UserGroup do
 
   index do
     id_column
-    column :group
+    column :group do |user_group|
+      title = user_group.group.title + ' ' + user_group.group.musical.title
+      title
+    end
     column :user
+    column :part
     column :status do |user_group| I18n.t("enum.user_group.status.#{user_group.status}") end
     column :created_at
     column :updated_at
@@ -17,8 +21,12 @@ ActiveAdmin.register UserGroup do
   show do
     attributes_table do
       row :id
-      row :group
+      row :group do |user_group|
+        title = user_group.group.title + ' ' + user_group.group.musical.title
+        title
+      end
       row :user
+      row :part
       row :status do |user_group| I18n.t("enum.user_group.status.#{user_group.status}") end
       row :created_at
       row :updated_at
@@ -27,10 +35,12 @@ ActiveAdmin.register UserGroup do
 
   form do |f|
     f.inputs do
-      f.input :group
+      f.input :group, as: :select, collection: Group.all.map {|g| [g.title + ' ' + g.musical.title, g.id ]}, input_html: { id: 'user_group_input' }
       f.input :user
+      f.input :part, as: :select, collection: Part.all.map {|part| [part.musical.title + ' ' + part.title, part.id ]}
       f.input :status, as: :select, collection: UserGroup.enum_selectors(:status)
     end
     f.actions
   end
+
 end
