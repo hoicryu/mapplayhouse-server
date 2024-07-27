b7e0f26b6c7f7289dc46b56e4bc96926b807c1a2
hoicryu <hoicryu@gmail.com>
2024-07-27 15:34:03 +0900

    [Added] user_groups rating counts to token

--- app/serializers/payload_serializer.rb
+++ app/serializers/payload_serializer.rb
@@ -1,6 +1,6 @@
 class PayloadSerializer < Panko::Serializer
   include ImagableSerializer
-  attributes :id, :name, :phone, :groups_i_applied, :groups_i_belong_to
+  attributes :id, :name, :phone, :groups_i_applied, :groups_i_belong_to, :num_of_participation_by_rating
 
   def groups_i_applied
     groups = object.application_forms.map {|form| form.group.id}
@@ -10,5 +10,13 @@ class PayloadSerializer < Panko::Serializer
   def groups_i_belong_to
     groups = object.groups.map {|group| group.id}
     groups
+  end
+
+  def num_of_participation_by_rating
+    statuses = Rating.statuses.keys
+    counts = statuses.each_with_object({}) { |status, hash| hash[status] = 0 }
+    ratings = object.user_groups.map { |user_group| user_group.part.rating.status }
+    ratings.each { |status| counts[status] += 1 }
+    counts
   end
 end
